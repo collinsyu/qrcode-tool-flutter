3031578794fafcc7f2f79306018235c5b3998d07
Anonymous <Anonymous@Anonymous.com>
2024-02-20 17:55:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 二维码工具app
+
+一个扫描二维码的工具app
+用flutter语言完成，目前仅支持android apk打包。
+## Getting Started
+
+解析二维码内容，生成二维码，小工具
---
+++ android/app/src/main/kotlin/com/hiking/tool/qrcode/qrcode/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hiking.tool.qrcode.qrcode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
